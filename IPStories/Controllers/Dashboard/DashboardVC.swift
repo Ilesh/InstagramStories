02b70581a6906ea42d3fa2403a1d6a63ef2ad9e0
Ile$h <ilesh.panchal1991@gmail.com>
2019-01-23 16:25:21 +0530

    - Add Navigation extension

--- IPStories/Controllers/Dashboard/DashboardVC.swift
+++ IPStories/Controllers/Dashboard/DashboardVC.swift
@@ -128,3 +128,30 @@ extension DashboardVC: HighlightsPostCellDelegate {
         }
     }
 }
+extension UINavigationController {
+    /**
+     It removes all view controllers from navigation controller then set the new root view controller and it pops.
+     
+     - parameter vc: root view controller to set a new
+     */
+    func initRootViewController(vc: UIViewController, transitionType type: String = kCATransitionFade, duration: CFTimeInterval = 0.3) {
+        self.addTransition(transitionType: type, duration: duration)
+        self.viewControllers.removeAll()
+        self.pushViewController(vc, animated: false)
+        self.popToRootViewController(animated: false)
+    }
+    
+    /**
+     It adds the animation of navigation flow.
+     
+     - parameter type: kCATransitionType, it means style of animation
+     - parameter duration: CFTimeInterval, duration of animation
+     */
+    private func addTransition(transitionType type: String = kCATransitionFade, duration: CFTimeInterval = 0.3) {
+        let transition = CATransition()
+        transition.duration = duration
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.type = type
+        self.view.layer.add(transition, forKey: nil)
+    }
+}
